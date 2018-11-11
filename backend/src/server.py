073d90222b43a5961d508f2f761756d3e37190c8
Miles McCain <0@rmrm.io>
2018-11-11 15:22:11 -0500

    Temporary fix for single VM

--- backend/src/server.py
+++ backend/src/server.py
@@ -18,4 +18,4 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-app.run(debug=False, host='0.0.0.0')
+app.run(debug=False, host='0.0.0.0', port=8080)
